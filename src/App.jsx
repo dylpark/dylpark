a17643cfc257641ded4b04fc4dff68207e443063
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-04-08 22:44:29 +1000

    Building out navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
       return (
-            <BrowserRouter className='relative z-0 bg-primary'>
-                  <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-                        <Navbar />
-                        <Hero />
+            <BrowserRouter>
+                  <div className='relative z-0 bg-primary'>
+                        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                              <Navbar />
+                              <Hero />
+                        </div>
+                        <About />
+                        <Experience />
+                        <Tech />
+                        <Works />
+                        <Feedbacks />
+                        <div className='relative z-0'>
+                              <Contact />
+                              <StarsCanvas />
+                        </div>
                   </div>
             </BrowserRouter>
       );
