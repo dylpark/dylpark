d8896b0e933bd1d5beb911d2c6ed1b4040b4024c
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-04-12 21:30:00 +1000

    Navbar v1 done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
       return (
             <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-                  <div className='w-full flex - justify-between items-center max-w-7xl mx-auto'>
+                  <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
                         <Link
                               to='/'
                               className='flex items-center gap-2'
@@ -22,8 +22,8 @@ const Navbar = () => {
                         >
                               <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
                               <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-                                    Dylan Park&nbsp;
-                                    <span className='sm:block hidden'>|&nbsp;Frontend Developer</span>
+                                    Dylan Park
+                                    <span className='sm:block hidden'>&nbsp;|&nbsp;Frontend Developer</span>
                               </p>
                         </Link>
                         <ul className='list-none hidden sm:flex flex-row gap-10'>
