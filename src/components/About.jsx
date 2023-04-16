c9ca40a1d55d159becda4d24667a013d19468baa
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-04-16 17:36:37 +1000

    Beginning about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import * as Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+// import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+     return (
+          <>
+               <motion.div>
+                    <p className={styles.sectionSubText}>introduction</p>
+                    <h2 className={styles.sectionHeadText}>Overview</h2>
+               </motion.div>
+          </>
+     );
+};
 
-export default About
+export default About;
