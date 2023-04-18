b360894465730755ce5c9ab6f710263646ee5bf8
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-04-18 22:26:20 +1000

    Creating about section with motion.js and constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,73 @@
 import React from 'react';
-import * as Tilt from 'react-tilt';
+import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-// import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 
+const ServiceCard = ({ index, title, icon }) => (
+    <Tilt className='xs:w-[250px] w-full'>
+        <motion.div
+            variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+            className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        >
+            <div
+                options={{
+                    max: 45,
+                    scale: 1,
+                    speed: 450,
+                }}
+                className='bg-tertiary rounded-[20px] py-5 px-12 
+                min-h-[280px] flex justify-evenly items-center flex-col'
+            >
+                <img
+                    src={icon}
+                    alt='web-development'
+                    className='w-16 h-16 object-contain'
+                />
+
+                <h3 className='text-white text-[20px] font-bold text-center'>
+                    {title}
+                </h3>
+            </div>
+        </motion.div>
+    </Tilt>
+);
+
 const About = () => {
-     return (
-          <>
-               <motion.div>
-                    <p className={styles.sectionSubText}>introduction</p>
-                    <h2 className={styles.sectionHeadText}>Overview</h2>
-               </motion.div>
-          </>
-     );
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Introduction</p>
+                <h2 className={styles.sectionHeadText}>Overview</h2>
+            </motion.div>
+            <motion.p
+                variants={fadeIn('', '', 0.1, 1)}
+                className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+            >
+                I’m based in Brisbane, Australia. I’m a self taught developer
+                with a degree in industrial design. I have a desire to create
+                things which has driven me to pursue a career in software
+                engineering.
+                <br />
+                <br />
+                Not long after graduating from university I started teaching
+                msyelf how to code. I’ve taken a liking to native iOS and
+                frontend web development. I'm currently working full-time as a
+                frontend engineer.
+            </motion.p>
+            <div className='mt-20 flex flex-wrap gap-10'>
+                {services.map((service, index) => (
+                    <ServiceCard
+                        key={service.title}
+                        index={index}
+                        {...service}
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default About;
---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+import { mobile, design, web } from '../assets';
+
+export const navLinks = [
+    {
+        id: 'about',
+        title: 'About',
+    },
+    {
+        id: 'work',
+        title: 'Work',
+    },
+    {
+        id: 'contact',
+        title: 'Contact',
+    },
+];
+
+const services = [
+    {
+        title: 'Frontend Engineer',
+        icon: web,
+    },
+    {
+        title: 'iOS Developer',
+        icon: mobile,
+    },
+    {
+        title: 'UI/UX Designer',
+        icon: design,
+    },
+];
+
+export { services };
