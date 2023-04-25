cb0785e0f936953e95400d8b4ac657b0b4505d52
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-04-25 22:07:00 +1000

    Assets and constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,22 @@
-import { mobile, design, web } from '../assets';
+import {
+    mobile,
+    web,
+    design,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    git,
+    figma,
+    docker,
+    threejs,
+    moonphase,
+    base,
+} from '../assets';
 
 export const navLinks = [
     {
@@ -17,7 +35,7 @@ export const navLinks = [
 
 const services = [
     {
-        title: 'Frontend Engineer',
+        title: 'Frontend Developer',
         icon: web,
     },
     {
@@ -25,9 +43,191 @@ const services = [
         icon: mobile,
     },
     {
-        title: 'UI/UX Designer',
+        title: 'Designer',
         icon: design,
     },
 ];
 
-export { services };
+const technologies = [
+    {
+        name: 'HTML 5',
+        icon: html,
+    },
+    {
+        name: 'CSS 3',
+        icon: css,
+    },
+    {
+        name: 'JavaScript',
+        icon: javascript,
+    },
+    {
+        name: 'TypeScript',
+        icon: typescript,
+    },
+    {
+        name: 'React JS',
+        icon: reactjs,
+    },
+    {
+        name: 'Redux Toolkit',
+        icon: redux,
+    },
+    {
+        name: 'Tailwind CSS',
+        icon: tailwind,
+    },
+    {
+        name: 'Node JS',
+        icon: nodejs,
+    },
+    {
+        name: 'Three JS',
+        icon: threejs,
+    },
+    {
+        name: 'git',
+        icon: git,
+    },
+    {
+        name: 'figma',
+        icon: figma,
+    },
+    {
+        name: 'docker',
+        icon: docker,
+    },
+];
+
+const experiences = [
+    {
+        title: 'Frontend Developer',
+        company_name: 'Company Name',
+        icon: base,
+        iconBg: '#383E56',
+        date: 'March 2020 - April 2021',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+    {
+        title: 'iOS Developer',
+        company_name: 'Company Name',
+        icon: base,
+        iconBg: '#E6DEDD',
+        date: 'Jan 2021 - Feb 2022',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+    {
+        title: 'Web Developer',
+        company_name: 'Company Name',
+        icon: base,
+        iconBg: '#383E56',
+        date: 'Jan 2022 - Jan 2023',
+        points: [
+            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.',
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            'I thought it was impossible to make a website as beautiful as ours, but Dylan proved me wrong.',
+        name: 'Tony Hawk',
+        designation: 'CEO',
+        company: 'Birdhouse',
+        image: 'https://cdn.shopify.com/s/files/1/1128/8094/files/TONY_HAWK_TEAMPAGE.jpg?17626671960250125766',
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Dylan does.",
+        name: 'Bert Kreischer',
+        designation: 'Tech Lead',
+        company: 'DEF Corp',
+        image: 'https://bertbertbert.com/wp-content/uploads/2022/06/Netflix-Pics-The-Cabin.png',
+    },
+    {
+        testimonial:
+            "After Dylan optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: 'David Choe',
+        designation: 'CTO',
+        company: '456 Enterprises',
+        image: 'https://images.squarespace-cdn.com/content/v1/559dbbb5e4b064657baefaaf/7fc5b2de-439b-4c66-b443-91d36eb19e5d/David+Choe+mixed+media+artwork.png?format=750w',
+    },
+];
+
+const projects = [
+    {
+        name: 'MoonPhase iOS',
+        description:
+            'Moon Phase is a minimalist, open-source, moon phase tracker. Moon Phase has been temporarily removed from the App Store due to issues with the API provider.',
+        tags: [
+            {
+                name: 'ios',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'cocoapods',
+                color: 'green-text-gradient',
+            },
+        ],
+        image: moonphase,
+        source_code_link: 'https://github.com/dylpark/MoonPhase-iOS',
+    },
+    {
+        name: 'MoonPhase iOS',
+        description:
+            'Moon Phase is a minimalist, open-source, moon phase tracker. Moon Phase has been temporarily removed from the App Store due to issues with the API provider.',
+        tags: [
+            {
+                name: 'react',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'restapi',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'tailwind',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: moonphase,
+        source_code_link: 'https://github.com/dylpark/MoonPhase-iOS',
+    },
+    {
+        name: 'MoonPhase iOS',
+        description:
+            'Moon Phase is a minimalist, open-source, moon phase tracker. Moon Phase has been temporarily removed from the App Store due to issues with the API provider.',
+        tags: [
+            {
+                name: 'nextjs',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'mongodb',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'css',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: moonphase,
+        source_code_link: 'https://github.com/dylpark/MoonPhase-iOS',
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
