7bf8adba99c61573265feeba65d77bb299cd03ce
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-05-20 18:05:35 +1000

    Hero and Navbar cleanup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,22 +10,17 @@ const Hero = () => {
                 className={`${styles.paddingX} absolute inset-0 top-[120px] 
                     max-w-7xl mx-auto flex flex-row items-start gap-5`}
             >
-                <div className='flex flex-col justify-center mt-5'>
-                    {/* <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-                    <div className='w-1 sm:h-80 h-40 violet-gradient' /> */}
+                <div className='flex flex-col justify-center items-center mt-5'>
+                    <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+                    <div className='w-1 sm:h-80 h-40 violet-gradient' />
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
                         Hi, I'm <span className='text-[#915eff]'>Dylan</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        My new site is coming soon.
+                        I'm a frontend developer with a design background.
                     </p>
-                    {/* <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I develop 3D visuals, user interfaces,
-                        <br className='sm:block hidden' /> iOS, and web
-                        applications.
-                    </p> */}
                 </div>
             </div>
             <ComputersCanvas />
