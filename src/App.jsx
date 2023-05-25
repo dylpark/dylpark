c2a90852e20548548108b30fa5831e4d6fc21656
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-05-25 17:01:53 +1000

    App.jsx cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
 import {
     About,
-    Contact,
     Experience,
-    Feedbacks,
     Hero,
     Navbar,
     Tech,
     Works,
     StarsCanvas,
-    ComingSoon,
 } from './components';
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className='relative z-0 bg-primary'>
-                <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                <div className='relative z-0'>
                     <Navbar />
                     <Hero />
+                    <StarsCanvas />
                 </div>
                 <About />
                 <Experience />
                 <Tech />
                 <Works />
-                {/* <Feedbacks /> */}
-                <div className='relative z-0'>
-                    <Contact />
-                    <StarsCanvas />
-                </div>
-            </div>
-            <div className='w-full h-full'>
-                {/* <ComingSoon /> */}
-                <StarsCanvas />
             </div>
         </BrowserRouter>
     );
