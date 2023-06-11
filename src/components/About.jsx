1d590e36ac1d850ee03ec5e349d9a7c7ba6f970a
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-06-11 15:41:33 +1000

    Converting ServiceCard to typescript

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -7,35 +6,7 @@ import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
 import { SectionWrapper } from '../hoc';
-
-const ServiceCard = ({ index, title, icon }) => (
-    <Tilt className='xs:w-[250px] w-full'>
-        <motion.div
-            variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-            className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-        >
-            <div
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450,
-                }}
-                className='bg-tertiary rounded-[20px] py-5 px-12 
-                min-h-[280px] flex justify-evenly items-center flex-col'
-            >
-                <img
-                    src={icon}
-                    alt='web-development'
-                    className='w-16 h-16 object-contain'
-                />
-
-                <h3 className='text-white text-[20px] font-bold text-center'>
-                    {title}
-                </h3>
-            </div>
-        </motion.div>
-    </Tilt>
-);
+import ServiceCard from '../ui-elements/ServiceCard';
 
 const About = () => {
     return (
